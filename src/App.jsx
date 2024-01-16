64a3d46c60ed74e6d24cebfe1455e799f87c2db2
Aryaman <aryamanyash11@gmail.com>
2024-01-16 21:23:34 +0530

    swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './component/header'
 import './App.css'
+import Swipers from './component/swiper'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Header/>
+     <Swipers/>
     </>
   )
 }
